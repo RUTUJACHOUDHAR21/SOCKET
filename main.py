db03066c4050d905d7af83d1b17e6f354ad7efb0
Rutuja choudhar <rutuja_choudhar_comp@moderncoe.edu.in>
2025-05-01 14:25:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,4 +48,4 @@ async def websocket_endpoint(websocket: WebSocket):
         manager.disconnect(websocket)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)

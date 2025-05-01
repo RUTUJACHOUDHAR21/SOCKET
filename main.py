04aa9b6a96532073a2a8d45e415661dfa659260d
Rutuja choudhar <rutuja_choudhar_comp@moderncoe.edu.in>
2025-05-01 13:35:40 +0530

    Update and rename server/server.js to  main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/static", StaticFiles(directory="."), name="static")
+
+@app.get("/")
+async def get():
+    with open("index.html") as f:
+        return HTMLResponse(f.read())
+
+clients = []
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for client in clients:
+                await client.send_text(data)
+    except:
+        clients.remove(websocket)
